94fbee7b97e20551d96d3365dedfacb634b3b9e0
Ranajit2005 <18ranajit2005@gmail.com>
2024-09-17 14:25:26 +0530

    profile

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -6,5 +6,22 @@ import { getDataFromToken } from "@/helpers/DataFromToken";
 connect(); //connecting to the database
 
 export async function POST(request: NextRequest){
+
     
+    const userId = await getDataFromToken(request); //get user id from the token
+    const user = await User.findOne({_id: userId}).select("-password"); //find the user on the basis of user id from database. We use select("-password") to avoid sending the password in the response
+
+    //If user is not found
+    if(!user){
+        return NextResponse.json({
+            message: "User not found",
+        })
+    }
+
+    //If user is found
+    return NextResponse.json({
+        message: "User found",
+        data: user
+    })
+
 }
