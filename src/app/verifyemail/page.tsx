e7b2fd61c2bf00b76d4c2b1178149fd9407005a0
Ranajit2005 <18ranajit2005@gmail.com>
2024-09-23 14:14:05 +0530

    Refactor component names, improve code formatting, and add email verification functionality

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -1,6 +1,34 @@
-import React from 'react'
+"use client"
+import React, {useEffect,useState} from 'react'
+import axios from 'axios'
+import {useRouter} from 'next/router'
 
 export default function VerifyEmailPage() {
+
+  const router = useRouter();
+
+  const [token,setToken] = useState("");  //We are taking the token from the user
+  const[verified,setVerified] = useState(false); //We are taking the verified from the user
+  const [error,setError] = useState(false); //We are taking the error from the user
+
+
+  const VerifyUseremail = async () => { 
+
+    try {
+
+      await axios.post("/api/users/verifyemail", {token}); //We are sending the token to the backend. We post method because in verifyemail of backend we use post method
+
+      setVerified(true); //If the email is verified then we set the verified to true
+      setError(false); //If the email is verified then we set the error to false
+      
+    } catch (error: any) {
+      setError(true);
+      console.log(error.response.data);
+    }
+
+  }
+
+
   return (
     <div>
       <h1>Verify Email</h1>
