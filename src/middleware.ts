21bd92e806d5d27b015358bbf1b36117b3137bf0
Ranajit2005 <18ranajit2005@gmail.com>
2024-09-24 20:59:06 +0530

    Refactor middleware to handle public and private paths based on token presence

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,29 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+
+  const path = request.nextUrl.pathname;    //It is inbluit function which is used to get the current path of the url
+
+  //Now there are two paths. public and private path.On the basis of cookies, we can different them.
+
+  const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail';   //Declera which paths are public paths
+
+  const token = request.cookies.get("token")?.value || "";   //It is used to get the token from the cookies.
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.url));   //If the token is present then it will redirect to the home page.
+  }
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.url));   //If the token is not present then it will redirect to the login page.
+  }
+
 }
- 
+
 // See "Matching Paths" below to learn more
+
 export const config = {
-  matcher: '/about/:path*',
-}
+  matcher: "/about/:path*",
+};
